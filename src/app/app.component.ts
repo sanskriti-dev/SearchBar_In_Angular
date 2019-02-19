90c327e499c44941b46338eaeeedb343da9011b9
Sanskriti Saluja <sanskriti.16bcs2076@abes.ac.in>
2019-02-19 21:06:50 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/observable';
+import 'rxjs/Rx';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit() {
+  }
+  form
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
